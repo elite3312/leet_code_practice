a7b43dbe129fadbf8303e6cc62e4ddbb95cbe3c8
Perry <perrywu12@gmail.com>
2022-09-04 14:57:05 +0800

    add list2tree

--- main.py
+++ main.py
@@ -1,50 +1,35 @@
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
 class Solution:
-    def numsSameConsecDiff(self, n: int, k: int) -> list[int]:
-        #2 <= n <= 9
-        #0 <= k <= 9
-
-        ans = {}
-        for i in range(1,10):
-            get_child(n, k, str(i), ans)
-        return list(ans.keys())
-
-
-def get_child(n: int, k: int, curr: str, res: list[int]):
-    if len(curr) == n:
-        res[curr] = 1
-        return
-    next = curr
-    
-    last_digit_of_next = next[-1]
-    last_digit_of_next = int(last_digit_of_next)
-
-    next_digit = int(str(last_digit_of_next+k))
-    if (next_digit < 10):
+    def verticalTraversal(self, root: TreeNode) -> list[list[int]]:
+        '''
+        The number of nodes in the tree is in the range [1, 1000].
+        0 <= Node.val <= 1000
+        '''
+        w=h=1000
+        Matrix = [[None for x in range(w)] for y in range(h)] 
+        r=0
+        c=0
+        s=[]
+        s.append(root)
+        index=0
+        while len(s):
+            curr=s.pop[-1]
+            #visit node
+            if curr is not None:
+                Matrix[r,c]=curr.val   
+            index+=1
             
-        next = next+str(next_digit)
-
-        get_child(n, k, next, res)
-    next = curr
- 
- 
-    last_digit_of_next = next[-1]
-    last_digit_of_next = int(last_digit_of_next)
-
-    next_digit = int(str(last_digit_of_next-k))
-    if (next_digit >= 0):
-            
-        next = next+str(next_digit)
-
-        get_child(n, k, next, res)
-
-
+            s.append(curr.right)
+            s.append(curr.left)
+        pass
 if __name__ == "__main__":
     s = Solution()
-    #n = 3  # len
-    #k = 4  # diff
-    #
-    #n=3
-    #k=7
-    n=2
-    k=1
-    print(s.numsSameConsecDiff(n, k))
+    root = [3,9,20,None,None,15,7]
+    from utils import binary_tree_from_list
+    root=binary_tree_from_list.to_binary_tree(root)
+    print(s.verticalTraversal(root))
