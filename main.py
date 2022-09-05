6ffbfa1961ec3ec206c2f97ee8580546e4f49988
elite3312 <perrywu12@gmail.com>
2022-09-05 10:48:01 +0800

    add 429

--- main.py
+++ main.py
@@ -1,53 +1,33 @@
-from utils import binary_tree_from_list
-# Definition for a binary tree node.
-class TreeNode:
-    def __init__(self, val=0, left=None, right=None):
+
+# Definition for a Node.
+
+# Definition for a Node.
+
+
+class Node:
+    def __init__(self, val=None, children=None):
         self.val = val
-        self.left = left
-        self.right = right
+        self.children = children
+
+
 class Solution:
-    def verticalTraversal(self, root: TreeNode) -> list[list[int]]:
-        '''
-        The number of nodes in the tree is in the range [1, 1000].
-        0 <= Node.val <= 1000
-        '''
-        d={}#dict that hashes (r,c)to a node
-        def inner(curr:TreeNode,r,c):
-            if curr is None:return
-            inner(curr.left,r+1,c-1)
-            inner(curr.right,r+1,c+1)
-            if (r,c) not in d.keys():
-                d[r,c]=[]
+    def levelOrder(self, root: Node) -> list[list[int]]:
+        if root is None:return []
+        q=[]
+        level=0
+        q.append((root,level))
+        ans=[]
+        while len(q):
+            curr=q.pop(0)
+
+            if len(ans)<curr[1]+1:
+                ans.append([])
+            ans[curr[1]].append(curr[0].val)
+
+            for c in curr[0].children:
+                q.append((c,curr[1]+1))
+        return ans
             
-            d[r,c].append(curr.val)
-            d[r,c].sort()
-        inner(root,0,0)
-        res=[]
-        
-        r=[]
-        c=[]
-        
-        for k in d.keys():
-            if k[0]not in r:r.append(k[0])
-            if k[1]not in c:c.append(k[1])
-        r.sort()
-        c.sort()
-        #print(d.keys())
-        #print(r)
-        #print(c)
-       
-        for col in c:
-            temp_list=[]
-            for row in r: 
-                try:
-                #if (row,col) in d.keys():
-                    for elem in d[(row,col)]:
-                        temp_list.append(elem)
-                except:pass
-            if len(temp_list)>0:
-                res.append(temp_list)
-        return res
-        
 if __name__ == "__main__":
     s = Solution()
     #root = [3,9,20,None,None,15,7]
