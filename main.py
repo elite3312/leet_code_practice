c9646ea6f65e1a9bfe1caa8494babd62d8be95a0
elite3312 <perrywu12@gmail.com>
2022-09-27 21:02:14 +0800

    838

--- main.py
+++ main.py
@@ -1,13 +1,35 @@
+
+
 class Solution:
-    def maxSubArray(self, nums):
-        for i in range(1, len(nums)):
-            nums[i] = max(nums[i-1] + nums[i], nums[i])
-        return max(nums)
-
-
+    def pushDominoes(self, dominoes: str) -> str:
+        ''' 
+        dominoes[i] = 'L', if the ith domino has been pushed to the left,
+        dominoes[i] = 'R', if the ith domino has been pushed to the right, and
+        dominoes[i] = '.', if the ith domino has not been pushed.
+        '''
+        dominoes=list(dominoes)
+        #edge 1:single elem
+        if len(dominoes)==1:return dominoes
+        s=set()
+        #edge 2:same dominoes for entire list
+        for d in dominoes:
+            s.add(d)
+        if len(s)==1:
+            return dominoes
+        right=[0]*len(dominoes)
+        left=[0]*len(dominoes)
+        distance=0
+        for i in range(len(dominoes)):
+            if dominoes[i]=='.':
+                distance+=1
+            
+            pass
+            
+        return ''.join(dominoes)
 if __name__ == "__main__":
 
     s = Solution()
-    
-    ans = s.maxSubArray([-2,1,-3,4,-1,2,1,-5,4])
+    #dominoes = "RR.L"
+    dominoes = ".L.R...LR..L.."
+    ans = s.pushDominoes(dominoes)
     print(ans)
