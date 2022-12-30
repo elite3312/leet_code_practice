90158e524559b56de3b53363aa05815f413b875c
elite3312 <perrywu12@gmail.com>
2022-12-30 14:56:23 +0800

    attempt

--- main.py
+++ main.py
@@ -1,45 +1,62 @@
 class Solution:
     def longestPalindrome(self, s: str) -> str:
-        is_substring=dict()#dp[_left,_right]=str[_left:_right+1] is palindrome or not
+        dp=dict()#dp[_left,_right]=true if s[_left,_right+1] is palindrome
         n=len(s)
         res=s[0]
         for i in range(n):
-            is_substring[(i,i)]=True
-        for _left in range(0,n):
-            for _right in range(_left,n):
+            dp[(i,i)]=(True,True,s[i])#(is_palinedrome, contains only one type of character), the only char
+        for left in range(0,n):
+            for right in range(0,n):
                 
-                try:
-                    if is_substring[(_left,_right)]==True:
-                        pass
-                except:
-                    try :
-                        if is_substring[(_left+1,_right-1)]:
-                            _set=set(s[_left+1,_right-1+1])
-                            if s[_left]==s[_right]:
-                                is_substring[(_left,_right)]=True
-                                if _right-_left+1>len(res):
-                                    res=s[_left:_right+1]
-                            else:
-                                is_substring[(_left,_right)]=False
-                            if len(_set)==1:
-                                _only_char=_set[0]
-                                if s[_left]==_only_char:
-                                    is_substring[(_left,_right-1)]=True
-                                if s[_right]==_only_char:
-                                    is_substring[(_left+1,_right)]=True
-
+                _candidate=dp.get((left,right))
+                if _candidate:
+                    if _candidate[0]==True:continue
+                    
+                else:
+                    _candidate_0=dp.get((left+1,right-1))
+                    
+                    if _candidate_0:#left+1,right-1
+                        if dp[(left+1,right-1)][1]:#all same
+                            _that_char= dp[(left+1,right-1)][2]
+                            if s[left]==s[right]==_that_char:
+                                dp[(left,right)]=(True,True,_that_char)
+                                if right-left+1>len(res):res=s[left:right+1]
+                            elif (s[left]==s[right])and (s[right]!=_that_char):
+                                dp[(left,right)]=(True,False,None)
+                                if right-left+1>len(res):res=s[left:right+1]
                             
-                        else:
-                            is_substring[(_left,_right)]=False
+                        else:#not all same
+                            if s[left]==s[right]:
+                                dp[(left,right)]=(True,False,None)
+                                if right-left+1>len(res):res=s[left:right+1]
+                    #############
+                    _candidate_1=dp.get((left,right-1))
+                    if _candidate_1:#left,right-1 
+                        if dp[(left,right-1)][1]:#all same      
+                            _that_char= dp[(left,right-1)][2]
+                            if s[right]==_that_char:
+                                dp[(left,right)]=(True,True,_that_char)
+                                if right-left+1>len(res):res=s[left:right+1]
+                    ##########
+                    _candidate_2=dp.get((left+1,right))
+                    if _candidate_2:#left+1,right
+                        if dp[(left+1,right)][1]:#all same   
+                            _that_char= dp[(left+1,right)][2]
+                            if s[left]==_that_char:
+                                dp[(left,right)]=(True,True,_that_char)
+                                if right-left+1>len(res):res=s[left:right+1]
+                    if not _candidate_0 and not _candidate_1 and not _candidate_2:
+                        dp[(left,right)]=(False,False,None)
                         
-                    except:
-                        pass
+        
                 
         return res
 if __name__ == "__main__":
     s=Solution()
-    #a="babad"
-    a="cbbd"
+    #a="babad"#bab
+    #a="cbbd"#bb
+    #a='bb'#bb
+    a="abcba"#"abcba"
     print(s.longestPalindrome(a))
     
     
