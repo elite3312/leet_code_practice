336339bdb2bac4608417905688c0800942da66fd
elite3312 <perrywu12@gmail.com>
2023-01-02 20:30:43 +0800

    refactor

--- move_file.py
+++ move_file.py
@@ -2,5 +2,5 @@ import shutil
 import sys
 import os
 if not os.path.exists('./'+sys.argv[1]+'.py'):
-    shutil.copyfile('./main.py','./'+sys.argv[1]+'.py')
+    shutil.copyfile('./main.py','./'+sys.argv[1]+'/'+sys.argv[2]+'.py')
 else:print('file exists')
