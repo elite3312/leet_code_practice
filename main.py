7fb3c1f66c04ea7c398634004a03cdf1084e4e4d
elite3312 <perrywu12@gmail.com>
2023-02-11 21:00:17 +0800

    49

--- main.py
+++ main.py
@@ -1,58 +1,23 @@
 class Solution:
-    def rotate(self, matrix: list[list[int]]) -> None:
-        """
-        Do not return anything, modify matrix in-place instead.
-        """
-        n=len(matrix)
-        side_len=n
-        for leftmost_row in range(n//2):
-            
-            for c_offset in range(side_len-1):
-
-                r=leftmost_row
-                c=leftmost_row+c_offset
-
-                val=matrix[r][c]
-                c+=side_len-1
-                # 01->14
-                if c_offset>0:
-                    r=leftmost_row+c_offset
-                    c=leftmost_row+(side_len-1)
-                temp=matrix[r][c]
-                matrix[r][c]=val
-                val=temp
-                
-                r+=side_len-1
-                # 14->43  
-                if c_offset>0:
-                    c=leftmost_row+(side_len-1)-c_offset
-                    r=leftmost_row+(side_len-1)
-                temp=matrix[r][c]
-                matrix[r][c]=val
-                val=temp
-
-                # 43->30
-                c-=side_len-1
-                if c<leftmost_row:
-                    r=leftmost_row+(side_len-1)-c_offset
-                    c=leftmost_row
-                temp=matrix[r][c]
-                matrix[r][c]=val
-                val=temp
-
-                # 30->03
-                r-=side_len-1
-                if r<leftmost_row:
-                    c=leftmost_row+c_offset
-                    r=leftmost_row
-                matrix[r][c]=val
-                    
-            side_len-=2
-
+    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
+        if len(strs)==1:
+            return [strs]
+        d=dict()
+        for s in strs:
+            _list=list(s)
+            _list.sort()
+            key=str(_list)
+            query_result=d.get(key)
+            if query_result==None:
+                d[key]=list()
+            d[key].append(s)
+        res=[]
+        for k in d:
+            res.append(d[k])
+        return res
 if __name__ == "__main__":
     sol = Solution()
-    #matrix=[[1,2,3],[4,5,6],[7,8,9]]
-    matrix=[[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
-    #matrix=[[1,2,3,4,5],[6,7,8,9,10],[11,12,13,14,15],[16,17,18,19,20],[21,22,23,24,25]]
-    sol.rotate(matrix)
-    print(matrix)
+    strs = ["eat","tea","tan","ate","nat","bat"]
+    res=sol.groupAnagrams(strs)
+    expected="expected: [[\"bat\"],[\"nat\",\"tan\"],[\"ate\",\"eat\",\"tea\"]]"
+    print(res,expected)
