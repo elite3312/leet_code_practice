1e85b9dccedda8f9ce254074a5a04df362fefc78
Perry <perrywu12@gmail.com>
2023-02-14 22:47:03 +0800

    try

--- main.py
+++ main.py
@@ -2,87 +2,55 @@ class Solution:
     def canJump(self, nums: list[int]) -> bool:
         n=len(nums)
         if n==1:return True
-        goal_lists=dict()#goal_lists[i] : a list containing possible starting jump points that go to land at i
-        for i in range(n):
-            goal_lists[i]=[]
-        goal_lists[0].append(0)
+        visited=[False]*n
+        
+        curr=0
+        while True:
+            visited[curr]=True
+            stride=nums[curr]
 
-        for index,elem in enumerate(nums[:n-1]):
-            can_reach_farthest=index+elem
-            if can_reach_farthest>=n-1:
-                can_reach_farthest=n-1
-            goal_lists[can_reach_farthest].append(index)
+            low=0 if curr-stride<0 else curr-stride
+            high=n-1 if curr+stride>n-1 else curr+stride
+            my_reach=range(low,high+1)
+            
+            index_2_jump=None
+            _max_stride=0
+            for i in my_reach:
+                if _max_stride<=nums[i] and not visited[i]:
+                    _max_stride=nums[i]
+                    index_2_jump=i
+            curr=index_2_jump
+            if curr==n-1:return True
+            elif nums[curr]==0:return False
 
-        #for _list in goal_lists:
-            #_list.sort()
         
 
-        curr_goal=n-1
-        prev_curr_goal=n
-        while True:
-            while True:
-                curr_max_reach=nums[curr_goal]
-                if len(goal_lists[curr_goal])!=0:
-                    break
-                else: curr_goal+=1
-                if curr_goal>curr_goal+curr_max_reach:
-                    return False
-                if curr_goal==prev_curr_goal:
-                    return False
-            prev_curr_goal=curr_goal
-            curr_goal=goal_lists[curr_goal][0]
-            if curr_goal==0:
-                return True
-            else:
-                if prev_curr_goal==curr_goal:
-                    return False
 
-def case_1():
+def test_case(input,expected):
     sol = Solution()
-    n=[2,3,1,1,4]
-    res=sol.canJump(n)
+    res=sol.canJump(input)
     expected=True
     print(res," ", expected)
-def case_2():
-    sol = Solution()
-    n=[3,2,1,0,4]
 
-    res=sol.canJump(n)
-    expected=False
-    print(res," ", expected)
-def case_3():
-    sol = Solution()
-    n=[2,0,1,1,4,2,1,1,10]
-
-    res=sol.canJump(n)
-    expected=True
-    print(res," ", expected)
-def case_4():
-    sol = Solution()
-    n=[0,2,3]
-
-    res=sol.canJump(n)
-    expected=False
-    print(res," ", expected)
-def case_5():
-    sol = Solution()
-    n=[3,2,1,0,4]
-
-    res=sol.canJump(n)
-    expected=False
-    print(res," ", expected)
-#[5,9,3,2,1,0,2,3,3,1,0,0]
-def case_6():
-    sol = Solution()
-    n=[5,9,3,2,1,0,2,3,3,1,0,0]
-
-    res=sol.canJump(n)
-    expected=True
-    print(res," ", expected)
 if __name__ == "__main__":
-    #case_1()
-    #case_2()
-    #case_3()
-    #case_4()
-    #case_5()
-    case_6()
+    t=[]
+    input=[2,3,1,1,4]
+    expected=True
+    t.append((input,expected))
+    input=[3,2,1,0,4]
+    expected=False
+    t.append((input,expected))
+    input=[2,0,1,1,4,2,1,1,10]
+    expected=True
+    t.append((input,expected))
+    input=[0,2,3]
+    expected=False
+    t.append((input,expected))
+    input=[3,2,1,0,4]
+    expected=False
+    t.append((input,expected))
+    input=[5,9,3,2,1,0,2,3,3,1,0,0]
+    expected=True
+    t.append((input,expected))
+    for test in t:
+        test_case(test[0],test[1])
