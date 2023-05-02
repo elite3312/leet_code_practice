53b093e3cb6384ba59c473b7948a206eb3698387
elite3312 <perrywu12@gmail.com>
2023-05-02 21:00:58 +0800

    update

--- 2d_array_init.py
+++ 2d_array_init.py
@@ -10,14 +10,15 @@ for row in range (r):
 print(board)
 print(transpose_board)
 
-board = [[1,2],
-         [3,4]]
-r = len(board)
-c = len(board[0])
-transpose_board = [[] for _ in range(c)]
-for row in range(r):
+board=[[1,2],
+       [3,4]]
+r=len(board)
+c=len(board[0])
+#transpose_board=[[0]*r]*c
+transpose_board=[[0 for _ in range(r)] for _ in range(c)]
+for row in range (r):
     for col in range(c):
-        transpose_board[col].append(board[row][col])
+        transpose_board[col][row]=board[row][col]
 
 print(board)
 print(transpose_board)
