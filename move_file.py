02eacb30bbe3c1e1eeb7b8baa8c5aaea476b4d8c
elite3312 <perrywu12@gmail.com>
2023-06-03 17:05:29 +0800

    121

--- move_file.py
+++ move_file.py
@@ -1,6 +1,9 @@
 import shutil
 import sys
 import os
-if not os.path.exists('./solutions/'+sys.argv[1]+'.py'):
-    shutil.copyfile('./main.py','./solutions/'+sys.argv[1]+'.py')
+file_name=sys.argv[1]
+file_name=file_name.replace(' ','_')
+file_name=file_name.replace('.','_')
+if not os.path.exists('./solutions/'+file_name+'.py'):
+    shutil.copyfile('./main.py','./solutions/'+file_name+'.py')
 else:print('file exists')
