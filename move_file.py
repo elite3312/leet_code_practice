893bcf77bdac1f52e5129bc70dba15b2ce3c6579
elite3312 <perrywu12@gmail.com>
2023-06-06 14:07:25 +0800

    rename files

--- move_file.py
+++ move_file.py
@@ -2,8 +2,7 @@ import shutil
 import sys
 import os
 file_name=sys.argv[1]
-file_name=file_name.replace(' ','_')
-file_name=file_name.replace('.','_')
+
 if not os.path.exists('./solutions/'+file_name+'.py'):
     shutil.copyfile('./main.py','./solutions/'+file_name+'.py')
 else:print('file exists')
