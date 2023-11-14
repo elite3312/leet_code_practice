29b5e114374c060f4af7fd409292aa641d6fc088
elite3312 <perrywu12@gmail.com>
2023-11-14 14:16:17 +0800

    208

---
+++ solutions/prefix_tree/208_Implement_Trie_Prefix_Tree.py
@@ -0,0 +1,49 @@
+class TrieNode:
+    def __init__(self):
+        # Stores children nodes and whether node is the end of a word
+        self.children = {}
+        self.isEnd = False
+
+class Trie:
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        cur = self.root
+        # Insert character by character into trie
+        for c in word:
+            # if character path does not exist, create it
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.isEnd = True
+        
+
+    def search(self, word: str) -> bool:
+        cur = self.root
+        # Search character by character in trie
+        for c in word:
+            # if character path does not exist, return False
+            if c not in cur.children:
+                return False
+            cur = cur.children[c]
+        return cur.isEnd
+        
+
+    def startsWith(self, prefix: str) -> bool:
+        # Same as search, except there is no isEnd condition at final return
+        cur = self.root
+        for c in prefix:
+            if c not in cur.children:
+                return False
+            cur = cur.children[c]
+        return True
+                                
+from utils.test_driver import test_driver
+if __name__ == "__main__":
+    s=Trie()
+    s.insert('apple')
+    test_driver(s.search,'apple',expected=True)
+    test_driver(s.startsWith,'ap',expected=True)
+    test_driver(s.startsWith,'p',expected=False)
+
