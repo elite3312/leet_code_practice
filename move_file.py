2acea46cca3ca2dce867fc755bc43e9dbf364136
Perry <perrywu12@gmail.com>
2023-12-03 20:48:25 +0800

    cpp

--- move_file.py
+++ move_file.py
@@ -1,13 +1,15 @@
 import shutil
 import sys
 import os
-file_names=sys.argv[1:]
+file_type=sys.argv[1]
+
+file_names=sys.argv[2:]
 for i in range(len(file_names)):
     file_names[i]=file_names[i].replace('_','')
     file_names[i]=file_names[i].replace(' ','_')
     file_names[i]=file_names[i].replace('.','')
     file_names[i]=file_names[i].replace('-','_')
 file_name='_'.join(file_names)
-if not os.path.exists('./solutions/'+file_name+'.py'):
-    shutil.copyfile('./main.py','./solutions/'+file_name+'.py')
+if not os.path.exists('./solutions/'+file_name+'.'+file_type):
+    shutil.copyfile('./main.'+file_type,'./solutions/'+file_name+'.'+file_type)
 else:print('file exists')
