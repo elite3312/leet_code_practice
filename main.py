d98e8bf38a2dc4b6b0d16c4df2bac76a47657c90
elite3312 <perrywu12@gmail.com>
2023-12-04 22:51:33 +0800

    coin change

--- main.py
+++ main.py
@@ -1,21 +1,30 @@
+from cmath import inf
 from utils.test_driver import test_driver
-class Solution:  
-    def lengthOfLIS(self, nums: list[int]) -> int:
-        n = len(nums)
-        dp = [1 for _ in n]
-        for i in range(n):
-            for j in range(i):  
-                if nums[i] > nums[j] and dp[i] < dp[j] + 1:
-                    dp[i] = dp[j] + 1
-        return max(dp)
+class Solution:
+    def coinChange(self, coins: list[int], amount: int) -> int:
+        n=len(coins)
+        
+        dp=[[0 for _ in range(n)]for _ in range(amount+1)]#dp[r,c]=min coins using 1st r coins to create amount c
+
+        for j in range(amount+1):
+            dp[0][j]=-1 if j %coins[0]!=0 else dp[0][j]//coins[0]
+        curr_min=inf
+        for i in range(1,n):
+            for j in range(1,amount+1):
+
+                dp[i][j]=min(dp[i-1][j],dp[i][j-1]+1)
+                curr_min=min(curr_min,dp[i][j])
+        return curr_min
+
+
+
+        
 if __name__ == "__main__":
     s=Solution()
     tests=[
-        [[10,9,2,5,3,7,101,18],4],
-        [[10],1],
-        [[10,10,10],1],
-        [[10,11,12],3],
-        [[4,10,4,3,8,9],3]
+        [[[1,2,5],11],3],
+        [[[2],3],-1],
+        [[[1],0],0],
     ]
     for a,b in tests:
-        test_driver(s.lengthOfLIS,a,expected=b)
+        test_driver(s.coinChange,a[0],a[1],expected=b)
