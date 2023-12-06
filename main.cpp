6da870165f432ac5e475305d39958dca4e9e3f00
elite3312 <perrywu12@gmail.com>
2023-12-06 16:05:05 +0800

    add  main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,32 @@
 #include <iostream>
 #include <vector>
 #include "utils/TestDriver.hpp"
+#include <limits>
 using namespace std;
 
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
+        if (amount==0)return 0;
         int n=coins.size();
-        return n;
+
+        int dp[12][10001]={0};
+        int Inf = numeric_limits<int>::max();  
+        for (int j =0 ;j<amount+1;j++){
+            dp[0][j]=j%coins[0]!=0 ? Inf:j/coins[0];
+        }
+        for (int i =1;i<n;i++)
+            for (int j=1 ;j <amount+1;j++){
+                if (coins[i]<=j)
+                    dp[i][j]=min(dp[i-1][j],dp[i][j-coins[i]]+1);
+                else
+                    dp[i][j]=dp[i-1][j];
+            }
+        int curr_min=Inf;
+        for (int i =0;i<n;i++){
+            curr_min=min(curr_min,dp[i][amount]);
+        }
+        return  curr_min==Inf ?-1: curr_min;
     }
 };
 int main (){
@@ -15,9 +34,7 @@ int main (){
     vector<int> v={1,2,5};
 
     int res=0;
-    res=s->coinChange(v,11);
-    
-    cout<<"ret:"<<res;
+    res=s->coinChange(v,100);
     
     return 0;
 }
