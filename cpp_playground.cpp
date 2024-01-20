eaea339bb67658ccf471509edaebbd659e27b0ec
elite3312 <perrywu12@gmail.com>
2024-01-20 22:05:43 +0800

    931 cpp

---
+++ cpp_playground.cpp
@@ -0,0 +1,29 @@
+#include "utils/linked_list.hpp"
+#include "utils/binary_tree.hpp"
+#include <iostream>
+#include <queue>
+#include <string>
+#include <vector>
+#include <unordered_map>
+#include <algorithm>
+#include <cstring>
+#include <math.h>
+#include <map>
+using namespace binary_tree;
+using namespace std;
+/**copy from here**/
+
+/**copy to here**/
+int main()
+{
+	int cur= min({1,2,3});
+	cout<<cur;
+
+	unordered_map<int,int>m;
+	m.insert(pair(1,2));
+	cout<<m.find(1)->second;
+	return 0;
+}
+
+/*idea
+*/
