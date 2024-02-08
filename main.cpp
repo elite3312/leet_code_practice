a0e3fdc461091cd2cde22dc32ff7928819fa810c
elite3312 <perrywu12@gmail.com>
2024-02-08 23:45:55 +0800

    update .vscode

--- main.cpp
+++ main.cpp
@@ -14,13 +14,18 @@ using namespace std;
 /**copy from here**/
 class Solution {
 public:
-    
+    int eraseOverlapIntervals(vector<vector<int> >& intervals) {
+        vector<pair<int,int>>v;
+		for (auto e:intervals)v.push_back({e[0],e[1]});
+		return 0;
+    }
 };
 /**copy to here**/
 int main()
 {
 	Solution *s = new Solution();
-	cout<<"123456";
+	vector<vector<int> > intervals={{1,2},{2,3},{3,4},{1,3}};
+	cout<<s->eraseOverlapIntervals(intervals);
 	return 0;
 }
 
