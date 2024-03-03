e619872ebc0af15bd46d76e79e12543a8c71542a
elite3312 <perrywu12@gmail.com>
2024-03-03 23:15:12 +0800

    some progressin rolling hash

--- main.cpp
+++ main.cpp
@@ -13,68 +13,44 @@ using namespace std;
 using namespace binary_tree;
 /**copy from here**/
 
-class Solution
-{
+class Solution {
 public:
-	vector<int> rolling_hash(string s, int window_size,
-							 int base = 26,
-							 int mod = 1000000007)
-	{
+    string shortestPalindrome(string s) {
+
+		/*find pal*/
+		int  base=23,mod=1e7+7;
+
 		int n = s.length();
 		vector<int> power(n + 1, 1);
-		vector<int> hash_values(n - window_size + 1);
-
 		// Precompute the powers of the base modulo the mod
-		for (int i = 1; i <= n; i++)
-		{
+		for (int i = 1; i <= n; i++) {
 			power[i] = (power[i - 1] * base) % mod;
 		}
 
-		// Compute the hash value of the first window
-		int current_hash = 0;
-		for (int i = 0; i < window_size; i++)
-		{
-			current_hash = (current_hash * base + s[i]) % mod;
+        /*compute hash for s*/
+		int hash=0,
+			hash_rev=0;
+		for(int i=0;i<n;i++){
+			/*compare hash value for window*/
+			hash=(hash+power[n-1-i]*s[i])%mod;
+			hash_rev=(hash_rev+power[i]*s[i])%mod;
 		}
-		hash_values[0] = current_hash;
 
-		// Compute the hash values of the rest of the substrings
-		for (int i = 1; i <= n - window_size; i++)
-		{
-			// Remove the contribution of the first character in
-			// the window
-			current_hash = (current_hash - power[window_size - 1] * s[i - 1]) % mod;
-
-			// Shift the window by one character and add the new
-			// character to the hash
-			current_hash = (current_hash * base + s[i + window_size - 1]) % mod;
-
-			hash_values[i] = current_hash;
+		int longest_paline_right=0;
+		for(int i=n-1;i>=0;i--){
+			if (hash==hash_rev){longest_paline_right=i;break;}
+			hash=(hash-power[n-1-i]*s[i])%mod;
+			hash_rev=(hash_rev-power[i]*s[i])%mod;
 		}
-		return hash_values;
-	}
-
-	
+		return s.substr(0,longest_paline_right+1);
+    }
 };
 /**copy to here**/
 int main()
 {
 	Solution *sol = new Solution();
-
-	string s = "abcabcabc";
- 
-    // Window size
-    int window_size = 3;
- 
-    // Calculate rolling hash values
-    vector<int> hash_values = sol->rolling_hash(s, window_size);
- 
-    // Print the hash values
-    for (auto& val : hash_values) {
-        cout << val << " ";
-    }
-    cout << endl;
- 
+	string s ="ababa";
+	cout<<sol->shortestPalindrome(s);
     return 0;
 }
 
