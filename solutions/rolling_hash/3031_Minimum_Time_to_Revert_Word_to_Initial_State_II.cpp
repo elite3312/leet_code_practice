0bd4d7bec8a01912f56a68229a2869d99cdab168
elite3312 <perrywu12@gmail.com>
2024-03-17 17:55:30 +0800

    3031 cpp

--- solutions/rolling_hash/3031_Minimum_Time_to_Revert_Word_to_Initial_State_II.cpp
+++ solutions/rolling_hash/3031_Minimum_Time_to_Revert_Word_to_Initial_State_II.cpp
@@ -8,39 +8,46 @@
 #include <math.h>
 #include <map>
 using namespace std;
-/**copy from here**/
-const int  base=23,mod=1e7+7;
+const int  base=23,base1=31,mod=1e7+7;
 class Solution {
 public:
 	
     int minimumTimeToInitialState(string word, int k) {
 		int res=0,n=word.size();
-
-		/*Precompute the powers of the base modulo the mod*/
-		vector<int> power(n, 1);
-		for (int i = 1; i < n; i++) {
-			power[i] = (power[i - 1] * base) % mod;
-		}
+		
 
 		/*compute forward_hashs for word*/
-		vector<long long> forward_hashs(n, 0);
-		forward_hashs[0]=word[0]*power[0]%mod;
-		for(int i = 1; i < n; i++)
-			forward_hashs[i]=(forward_hashs[i-1]+word[i]*power[i])%mod;
+		long long forward_hash=0,forward_hash1=0,pow=1,pow1=1;
+		map<pair<int,int>,bool>h;
 		
-		/*same logic from 3029*/
-		int right_len=n,right_hash=forward_hashs[n-1];
-		while(1){
+		for(int i = 0; i < n; i++){
+			forward_hash=(forward_hash+word[i]*pow)%mod;
+			pow=pow*base%mod;
+			forward_hash1=(forward_hash1+word[i]*pow1)%mod;
+			pow1=pow1*base1%mod;
+			h[{forward_hash,forward_hash1}]=true;
+		}
+
+		// creating isprefix array that tells if the substring word.substr(i) is also a prefix of word or not
+		vector<bool>isprefix(n,false);
+		long long backward_hash=0,backward_hash1=0;
+        for(int i = n-1;i>=0;i--){
+            backward_hash1 = (backward_hash1*base1+word[i])%mod;
+            backward_hash = (backward_hash*base+word[i])%mod;
+            if(h.find({backward_hash,backward_hash1})!=h.end())isprefix[i]=true;
+			
+        }
+
+		int remaining_right_chars_ptr=0;
+		do{
 			res++;
-			if (right_len-1>=k){
-				right_len-=k;
-				for(int i=0;i<k;i++)
-					right_hash=(right_hash-word[n-right_len+i]*power[n-right_len+i])/base%mod;
-			}
+			if ((n-1-remaining_right_chars_ptr)>=k)
+				remaining_right_chars_ptr+=k;
 			else break;
 			
-			if (forward_hashs[right_len-1]==right_hash)break;
 		}
+		while (isprefix[remaining_right_chars_ptr]==false);
+		
 		
 		return res;
     }
