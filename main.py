0138ac2ea7b2868e5f8ceab30a32c5b5e33ad930
elite3312 <perrywu12@gmail.com>
2024-04-08 17:36:02 +0800

    1700+

--- main.py
+++ main.py
@@ -1,39 +1,21 @@
 from utils.test_driver import test_driver
 
-import heapq
-
-
-class CustomItem:
-    def __init__(self, priority:list, value):
-        self.priority = priority
-        self.value = value
-
-    def __lt__(self, other):
-        return self.priority[0] >= other.priority[0]
-    
 class Solution:
-    def mostFrequentIDs(self, nums: list[int], freq: list[int]) -> list[int]:
-        cnter=dict()
-        l=[]#(obj(cnt),val)
-        n=len(nums)
-
-        res=[]
-        
-        for i in range(n):
-            is_first_time_2_add=False
-            if cnter.get(nums[i])==None:
-                cnter[nums[i]]=[0]
-                is_first_time_2_add=True
-
-            cnter[nums[i]][0]+=freq[i]
-            if cnter[nums[i]][0]<0:cnter[nums[i]][0]=0
-            
-            if is_first_time_2_add:
-                heapq.heappush(l,CustomItem(cnter[nums[i]],nums[i]))
-      
-            else:heapq.heapify(l)
-
-            res.append(l[0].priority[0])
+    def countStudents(self, students: list[int], sandwiches: list[int]) -> int:
+        i=0
+        n=len(students)
+        res=n
+        rotate_cnt=0
+        while rotate_cnt<n:
+            if students[i]==sandwiches[i]:
+                sandwiches.pop(0)
+                students.pop(0)
+                res-=1
+                if len(students)==0:break
+            else:
+                students=students[1:]
+                students.append(students[0])
+                rotate_cnt+=1
         return res
 
         
@@ -44,22 +26,22 @@ if __name__ == "__main__":
         [
             # inputs
             [
-                [5,5,3],[2,-2,1]
+                [1,1,0,0], [0,1,0,1]
             ],
             # res
-            [2,0,1]
+            0
 
         ],
         [
             # inputs
             [
-                [2,3,2,1],[3,2,-3,1]
+                [1,1,1,0,0,1], [1,0,0,0,1,1]
             ],
             # res
-            [3,3,2,2]
+            3
 
         ],
     
     ]
     for input, res in tests:
-        test_driver(s.mostFrequentIDs, input[0],input[1],  expected=res)
+        test_driver(s.countStudents, input[0],input[1],  expected=res)
