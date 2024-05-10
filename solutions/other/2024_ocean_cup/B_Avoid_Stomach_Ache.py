e12689e012f715719881ea39ce1d03817063ce68
elite3312 <perrywu12@gmail.com>
2024-05-10 10:06:43 +0800

    complete B_Avoid_Stomach_Ache

--- solutions/other/2024_ocean_cup/B_Avoid_Stomach_Ache.py
+++ solutions/other/2024_ocean_cup/B_Avoid_Stomach_Ache.py
@@ -15,7 +15,7 @@ class Solution:
         a_min,a_max=1,2e9+87
         b_min,b_max=1,2e9+87
         c_min,c_max=1,2e9+87
-        c_max_changed=False
+
         # set lower bounds
         for r in records:
             if r[-1]==False:
@@ -27,30 +27,24 @@ class Solution:
             if r[-1]==True:
                 if r[0]>=a_min:a_max=min(a_max,r[0])
                 if r[1]>=b_min:b_max=min(b_max,r[1])
-                if (r[0]+r[1])>=c_min:
-                    c_max_changed=True
-                    c_max=min(c_max,r[0]+r[1])
+                if (r[0]+r[1])>=c_min:c_max=min(c_max,r[0]+r[1])
 
-        a,b,c=None,None,None
-        if a_max==a_min:
-            a=a_max
-            b=b_min
-            c=c_min
-        elif b_max==b_min:
-            b=b_max
-            a=a_min
-            c=c_min
-        elif c_max==c_min:
-            c=c_max
-            a=a_min
-            b=b_min
-        if a==None and b==None and c==None:
-            if c_max_changed:
-                a=a_min
-                b=b_min
-                c=c_max
+        a=min(a_min,a_max)
+        
+        b=min(b_min,b_max)
+        
+        c=min(c_min,c_max)
+  
+        # sanity check
+        for r in records:
+            _failed=False
+            if r[-1]==True:
+                if r[0]<a and r[1]<b and (r[0]+r[1])<c:_failed=True
+                if _failed:return [-1]
             else:
-                return [-1]
+                if r[0]>=a or r[1]>=b or (r[0]+r[1])>=c:_failed=True
+                if _failed:return [-1] 
+
         return [a,b,c]
 
 
@@ -62,35 +56,41 @@ if __name__ == "__main__":
 
     if not submit:
         tests = [
-            [
+            [#0
                 [[1, 2, True],
                  [2, 1, True],
                  [5, 0, False]],
                 [6, 1, 6],
             ],
-            [
+            [#1
                 [[1, 0 ,True],
                  [0, 1 ,True]],
                 [1,1,1],
             ],
-            [
+            [#2
                 [[1000000000, 1000000000 ,True],
                  [0, 0 ,True]],
                 [-1],
             ],
-            [
+            [#3
                 [[8, 0, True],
                  [7, 0, False],
                  [3, 5, False],
                  [6, 8, True]],
-                [8, 8, 14],
+                [8, 6, 9],
             ],
-            [
+            [#4
                 [
                  [100, 0, False],
                  [60, 60, True],
                  [0, 100, False]],
-                [-1],
+                [101, 101, 101],
+            ],
+            [#5
+                [
+                 [0, 1, False],
+                 [1, 0, False],],
+                [2,2,2],
             ],
         ]
         for input, res in tests[index:]:
