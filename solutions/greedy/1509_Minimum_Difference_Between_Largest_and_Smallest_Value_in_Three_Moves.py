c34541985176c2e21ea63f4b71a16fdf6683b6d9
elite3312 <perrywu12@gmail.com>
2024-07-04 00:07:29 +0800

    1509 alt

--- solutions/greedy/1509_Minimum_Difference_Between_Largest_and_Smallest_Value_in_Three_Moves.py
+++ solutions/greedy/1509_Minimum_Difference_Between_Largest_and_Smallest_Value_in_Three_Moves.py
@@ -23,7 +23,7 @@ class Solution:
                     l.append(nums[i])
             diffs.append(abs(l[0]-l[-1]))
         return  min(diffs)
-    def minDifference(self, nums: list[int]) -> int:
+    def minDifference_1(self, nums: list[int]) -> int:
         if len(nums) <= 4:
             return 0
         nums.sort()
@@ -37,6 +37,44 @@ class Solution:
         '''
         for i in range(4):
             ans = min(ans, nums[-(4 - i)] - nums[i])
+        return ans
+    def minDifference(self, nums: list[int]) -> int:
+        def shift(arr:list,left_index:int):
+            n=len(arr)
+            for i in range(n-1,left_index,-1):
+                arr[i]=arr[i-1]
+        
+        if len(nums) <= 4:
+            return 0
+        
+        min4 = [float('inf')] * 4
+        max4 = [float('-inf')] * 4
+
+        for num in nums:
+            added = False
+            for j in range(4):
+                if num < min4[j]:
+                    shift(min4, j)
+                    min4[j] = num
+                    added = True
+                    break
+            if not added and min4[-1] == float('inf'):
+                min4[-1] = num
+        for num in nums:
+            added = False
+            for j in range(4):
+                if num > max4[j]:
+                    shift(max4, j)
+                    max4[j] = num
+                    added = True
+                    break
+            if not added and max4[-1] == float('-inf'):
+                max4[-1] = num
+        
+        ans = max4[0] - min4[0]
+        for i in range(4):
+            ans = min(ans, max4[3 - i] - min4[i])
+        
         return ans
 if __name__ == "__main__":
     sol = Solution()
