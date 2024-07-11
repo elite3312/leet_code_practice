9d9d0aa5d671f79684a1bee8effa27daed18ab0b
Perry <perrywu12@gmail.com>
2024-07-11 17:26:58 +0800

    temp

--- main.py
+++ main.py
@@ -5,41 +5,57 @@ from utils.test_driver import test_driver_main
 # from collections import deque
 
 class Solution:
-    def minOperations(self, logs: list[str]) -> int:
-        depth=0
-        for log in logs:
-            if log=='./':
-                pass
-            elif log=='../':
-                if depth>0:
-                    depth-=1
-            else:
-                depth+=1
-        return depth
+    def reverseParentheses(self, s: str) -> str:
+        
+        rev=[]
+        res=[]
+        left_brac_cnt=0
 
+        stop_i=None
+        for i,c in enumerate(s):
+            if c == '(':
+                left_brac_cnt+=1
+            elif c==')':
+                if not stop_i:
+                    stop_i=i
+            else:
+                res.append(c)
+                if stop_i:
+                    rev.append(False)
+                else:
+                    if left_brac_cnt%2==1:
+                        rev.append(True)
+                    else:
+                        rev.append(False)
+        n=len(res)
+        swapped=[False for i in range(n)]
+        for i in range(n):
+            if rev[i] and not swapped[i]:
+                temp=res[-(i+1)]
+                res[-(i+1)]=res[i]
+                res[i]=temp
+                swapped[-(i+1)]=True
+        
+        return ''.join(res)
 if __name__ == "__main__":
     sol = Solution()
 
-    index = 0
+    index = 1
 
     tests = [
         [[
-            ["d1/","d2/","../","d21/","./"]
+           "(abcd)"
         ],
-           2
-        ],
-        [[
-            ["d1/","d2/","./","d3/","../","d31/"]
-        ],
-           3
+           "dcba"
         ],
         [[
-            ["d1/","../","../","../"]
+           "(ed(et(oc))el)"
         ],
-           0
+           "leetcode"
         ],
+       
         
 
     ]
 
-    test_driver_main(sol.minOperations, tests, index)
+    test_driver_main(sol.reverseParentheses, tests, index)
